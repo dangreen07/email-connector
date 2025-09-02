1d0529d6b5d2cbc7a39ed37445d0a86c95014543
Daniel Green <dangreen07@outlook.com>
2025-09-02 13:55:40 +0100

    Added API docs

--- frontend/app/docs/page.tsx
+++ frontend/app/docs/page.tsx
@@ -1,3 +1,24 @@
-export default function docs() {
-  return <div></div>;
+"use client";
+import { useTheme } from "next-themes";
+import { useEffect } from "react";
+import { RedocStandalone } from "redoc";
+
+export default function DocsPage() {
+  const { setTheme, theme } = useTheme();
+
+  useEffect(() => {
+    // Theme stays in light mode
+    setTheme("light");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [theme]);
+  return (
+    <div>
+      <RedocStandalone
+        specUrl="/openapi.yaml"
+        options={{
+          hideDownloadButton: true,
+        }}
+      />
+    </div>
+  );
 }
